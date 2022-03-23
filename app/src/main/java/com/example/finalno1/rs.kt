c9cee67138aaf84606746b03a301366f294cb845
outaa96 <636051200146@mail.rmutk.ac.th>
2022-03-23 16:55:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/finalno1/rs.kt
@@ -0,0 +1,11 @@
+package com.example.finalno1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class rs : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rs)
+    }
+}
